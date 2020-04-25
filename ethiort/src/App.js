432bef7d01a8ca5be514d98c3dbb1f31ce3aca2d
azdemissiew <ademssiew@gmail.com>
2020-04-24 22:59:44 -0700

    adde sign in and sign up component

--- ethiort/src/App.js
+++ ethiort/src/App.js
@@ -3,7 +3,9 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch,Route} from "react-router-dom";
 import './App.css';
 import shopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
     <Route exact path= '/' component={HomePage} />
     <Route path= '/shop' component={shopPage} />
+    <Route path= '/signin' component={SignInAndSignUpPage} />
     </Switch>
     </div>
   );
