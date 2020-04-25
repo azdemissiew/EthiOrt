1df02bd5329a5ddd36f3cd6299e2e467acb4fe48
azdemissiew <ademssiew@gmail.com>
2020-04-24 17:41:39 -0700

    added header component and links to home, shop page and contact

--- ethiort/src/App.js
+++ ethiort/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-import {Route} from "react-router-dom";
+import {Switch,Route} from "react-router-dom";
 import './App.css';
 import shopPage from './pages/shop/shop.component';
-
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <switch>
+      <Header />
+      <Switch>
     <Route exact path= '/' component={HomePage} />
     <Route path= '/shop' component={shopPage} />
-    </switch>
+    </Switch>
     </div>
   );
 }
